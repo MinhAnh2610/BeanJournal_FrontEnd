bd2d6e7a3c605ba58edbd970dd433083587df6ab
Phạm Hoàng Minh Anh <anhphmse183496@fpt.edu.vn>
2024-09-02 13:06:39 +0700

    Menubar at homepage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,28 @@
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
+import { Link } from "react-router-dom";
+
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <div>
+      <Menubar>
+        <MenubarMenu>
+          <MenubarTrigger>File</MenubarTrigger>
+          <MenubarContent>
+            <MenubarItem><Link to="/login">Login</Link></MenubarItem>
+            <MenubarItem><Link to="/register">Register</Link></MenubarItem>
+          </MenubarContent>
+        </MenubarMenu>
+      </Menubar>
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
