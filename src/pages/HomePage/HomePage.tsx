148f3054b67897d5440d62eb89a596638cce006e
Phạm Hoàng Minh Anh <anhphmse183496@fpt.edu.vn>
2024-09-03 23:21:58 +0700

    HomePage temporary

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,24 +1,48 @@
-import {
-  Menubar,
-  MenubarContent,
-  MenubarItem,
-  MenubarMenu,
-  MenubarTrigger,
-} from "@/components/ui/menubar";
 import { Link } from "react-router-dom";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Button,
+} from "@nextui-org/react";
+import Logo from "@/components/custom/Logo";
 
 const HomePage = () => {
   return (
-    <div>
-      <Menubar>
-        <MenubarMenu>
-          <MenubarTrigger>File</MenubarTrigger>
-          <MenubarContent>
-            <MenubarItem><Link to="/login">Login</Link></MenubarItem>
-            <MenubarItem><Link to="/register">Register</Link></MenubarItem>
-          </MenubarContent>
-        </MenubarMenu>
-      </Menubar>
+    <div className="h-screen w-screen">
+      <Navbar shouldHideOnScroll>
+        <NavbarBrand>
+          <Logo />
+        </NavbarBrand>
+        <NavbarContent className="hidden sm:flex gap-4" justify="center">
+          <NavbarItem>
+            <Link color="foreground" to="/features">
+              Features
+            </Link>
+          </NavbarItem>
+          <NavbarItem isActive>
+            <Link to="/customers" aria-current="page">
+              Customers
+            </Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link color="foreground" to="/integrations">
+              Integrations
+            </Link>
+          </NavbarItem>
+        </NavbarContent>
+        <NavbarContent justify="end">
+          <NavbarItem className="hidden lg:flex">
+            <Link to="/login">Login</Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Button as={Link} color="primary" to="/register" variant="flat">
+              Sign Up
+            </Button>
+          </NavbarItem>
+        </NavbarContent>
+      </Navbar>
     </div>
   );
 };
