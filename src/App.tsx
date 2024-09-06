07bb1264ebe1973f504053b1f52d8bce1945c4eb
Phạm Hoàng Minh Anh <anhphmse183496@fpt.edu.vn>
2024-09-06 12:54:18 +0700

    Text sizing for phone intro page

--- src/App.tsx
+++ src/App.tsx
@@ -16,26 +16,31 @@ function App() {
         opacity: 0,
         y: "+=30",
         stagger: 0.5,
-      }).to(["#title-1", "#title-2", "#title-3"], {
-        opacity: 0,
-        y: "-=30",
-        delay: 0.3,
-        stagger: 0.5,
-      }).to("#intro-slider", {
-        xPercent: -100,
-        duration: 1.3,
-      }).from("#welcome", {
-        opacity: 0,
-        duration: 1.0,
-        delay: 0.5,
-      }).to("#welcome", {
-        opacity: 0,
-        duration: 1.0,
-        delay: 0.3
-      }).to("#welcome-slider", {
-        xPercent: -100,
-        duration: 1.3,
-      });
+      })
+        .to(["#title-1", "#title-2", "#title-3"], {
+          opacity: 0,
+          y: "-=30",
+          delay: 0.3,
+          stagger: 0.5,
+        })
+        .to("#intro-slider", {
+          xPercent: -100,
+          duration: 1.3,
+        })
+        .from("#welcome", {
+          opacity: 0,
+          duration: 1.0,
+          delay: 0.5,
+        })
+        .to("#welcome", {
+          opacity: 0,
+          duration: 1.0,
+          delay: 0.3,
+        })
+        .to("#welcome-slider", {
+          xPercent: -100,
+          duration: 1.3,
+        });
     }, comp);
     return () => ctx.revert();
   }, []);
@@ -50,20 +55,32 @@ function App() {
       </NextUIProvider>
       <div
         id="intro-slider"
-        className="h-screen bg-colour-lavender absolute top-0 left-0 z-50 w-full flex gap-5 justify-center place-items-center tracking-tight"
+        className="h-screen bg-colour-lavender absolute top-0 left-0 z-50 w-full flex gap-5 justify-center lg:place-items-center tracking-tight pl-24 flex-col lg:flex-row lg:pl-0"
       >
-        <h1 className="text-colour-indigo text-7xl font-bold" id="title-1">
+        <h1
+          className="text-colour-indigo text-md font-bold lg:text-6xl"
+          id="title-1"
+        >
           Capture
         </h1>
-        <h1 className="text-colour-indigo text-7xl font-bold" id="title-2">
+        <h1
+          className="text-colour-indigo text-md font-bold lg:text-6xl"
+          id="title-2"
+        >
           Reflect
         </h1>
-        <h1 className="text-colour-indigo text-7xl font-bold" id="title-3">
+        <h1
+          className="text-colour-indigo text-md font-bold lg:text-6xl"
+          id="title-3"
+        >
           Grow
         </h1>
       </div>
-      <div id="welcome-slider" className="h-screen bg-white absolute top-0 left-0 w-full flex justify-center place-items-center">
-        <h1 id="welcome" className="text-9xl font-bold">
+      <div
+        id="welcome-slider"
+        className="h-screen bg-white absolute top-0 left-0 w-full flex justify-center place-items-center"
+      >
+        <h1 id="welcome" className="text-md lg:text-9xl font-bold">
           Bean Journal
         </h1>
       </div>
