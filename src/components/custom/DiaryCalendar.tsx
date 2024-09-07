d025095bf59e0223062230a4dbb7e73728ac7834
Phạm Hoàng Minh Anh <anhphmse183496@fpt.edu.vn>
2024-09-08 00:16:13 +0700

    Diary Calendar

--- src/components/custom/DiaryCalendar.tsx
+++ src/components/custom/DiaryCalendar.tsx
@@ -1,14 +1,61 @@
-import { Calendar } from "@nextui-org/react";
+import { useEffect, useState } from "react";
+import { useCalendarApp, ScheduleXCalendar } from "@schedule-x/react";
+import { createViewMonthGrid } from "@schedule-x/calendar";
+
+import "@schedule-x/theme-default/dist/index.css";
 
 const DiaryCalendar = () => {
+  const [selectedDate, setSelectedDate] = useState("2024-09-07");
+  const [events, setEvents] = useState([
+    {
+      id: "1",
+      title: "My new event",
+      start: "2024-09-09",
+      end: "2024-09-09",
+    },
+    {
+      id: "2",
+      title: "My new event",
+      start: "2024-09-07",
+      end: "2024-09-07",
+    },
+    {
+      id: "3",
+      title: "My new event",
+      start: "2024-09-08",
+      end: "2024-09-08",
+    },
+    {
+      id: "4",
+      title: "My new event",
+      start: "2024-09-10",
+      end: "2024-09-10",
+    },
+    {
+      id: "5",
+      title: "My new event",
+      start: "2024-09-11",
+      end: "2024-09-11",
+    },
+  ]);
+
+  const calendar = useCalendarApp({
+    views: [createViewMonthGrid()],
+    events,
+    selectedDate,
+  });
+
+  useEffect(() => {
+    setEvents([]);
+    setSelectedDate("2024-09-07");
+  }, [events]);
+
   return (
     <div>
-      <Calendar
-        weekdayStyle="long"
-        showMonthAndYearPickers
-      />
+      <ScheduleXCalendar calendarApp={calendar} />
     </div>
   );
 };
 
 export default DiaryCalendar;
+
