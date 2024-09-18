572e88492f769007144ee1758a684ba1c8b7d2ed
Phạm Hoàng Minh Anh <anhphmse183496@fpt.edu.vn>
2024-09-19 00:39:57 +0700

    Diary Get API

--- src/components/custom/DiaryCalendar/DiaryCalendar.tsx
+++ src/components/custom/DiaryCalendar/DiaryCalendar.tsx
@@ -1,50 +1,42 @@
-import { useEffect, useState } from "react";
+// import { useLayoutEffect, useState } from "react";
 import { useCalendarApp, ScheduleXCalendar } from "@schedule-x/react";
 import { createViewMonthGrid } from "@schedule-x/calendar";
 
 import "@schedule-x/theme-default/dist/index.css";
 import "../DiaryCalendar/DiaryCalendar.css";
-import { GetDiaryEntriesAPI } from "@/services/DiaryEntryService";
-import { toast } from "sonner";
-import { EventGet } from "@/models/Event";
+// import { EventGet } from "@/models/Event";
 import { DiaryEntryGet } from "@/models/DiaryEntry";
 
-const DiaryCalendar = () => {
-  const [diaries, setDiaries] = useState<DiaryEntryGet[]>([]);
-  const [events, setEvents] = useState<EventGet[]>([]);
+type Props = {
+  diaries: DiaryEntryGet[];
+};
 
-  const getDiaries = async () => {
-    await GetDiaryEntriesAPI()
-      .then((res) => {
-        if (res?.data) {
-          setDiaries(res.data);
-        }
-      })
-      .catch((err) => {
-        toast.error(err);
-      });
-  };
+const DiaryCalendar = ({ diaries }: Props) => {
+  // const [events, setEvents] = useState<EventGet[]>([]);
 
-  useEffect(() => {
-    getDiaries();
-  }, []);
-
-  useEffect(() => {
-    setEvents(
-      diaries.map((diary) => {
-        return {
-          id: diary.entryId,
-          title: diary.title,
-          start: diary.createdAt,
-          end: diary.updatedAt,
-        };
-      })
-    );
-  }, [diaries]);
+  // useLayoutEffect(() => {
+  //   setEvents(
+  //     diaries.map((diary) => {
+  //       return {
+  //         id: diary.entryId,
+  //         title: diary.title,
+  //         start: diary.createdAt,
+  //         end: diary.updatedAt,
+  //       };
+  //     })
+  //   );
+  // }, []);
 
   const calendar = useCalendarApp({
     views: [createViewMonthGrid()],
-    events: events,
+    events: diaries.map((diary) => {
+      return {
+        id: diary.entryId,
+        title: diary.title,
+        start: diary.createdAt,
+        end: diary.updatedAt,
+      };
+    })
   });
 
   return (
