4c0185cab8a67f79a6930a42b74c9f69a1c36017
Phạm Hoàng Minh Anh <anhphmse183496@fpt.edu.vn>
2024-10-22 16:49:56 +0700

    Add sections for the home page

---
+++ src/components/custom/HomePageComponents/Testemonials.tsx
@@ -0,0 +1,7 @@
+const Testemonials = () => {
+  return (
+    <div>Testemonials</div>
+  )
+}
+
+export default Testemonials
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,34 @@
 import { Link } from "react-router-dom";
 import Logo from "@/components/custom/UtilityComponents/Logo";
+import Hero from "@/components/custom/HomePageComponents/Hero";
+import Feature from "@/components/custom/HomePageComponents/Feature";
+import Demo from "@/components/custom/HomePageComponents/Demo";
+import Testemonials from "@/components/custom/HomePageComponents/Testemonials";
+import Footer from "@/components/custom/HomePageComponents/Footer";
 
 const HomePage = () => {
   return (
-    <div className="flex justify-center items-center gap-10 p-4">
-      <Logo />
-      <Link to="/login">Login</Link>
-      <Link to="/register">Register</Link>
+    <div className="px-40">
+      <div className="flex justify-center items-center gap-10 p-4">
+        <Logo />
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
+      </div>
+      <div>
+        <Hero />
+      </div>
+      <div>
+        <Feature />
+      </div>
+      <div>
+        <Demo />
+      </div>
+      <div>
+        <Testemonials />
+      </div>
+      <div>
+        <Footer />
+      </div>
     </div>
   );
 };
